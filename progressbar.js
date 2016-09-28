de757a14acb9bf9b153da65bff0878f88b14b452
John-Paul Robinson <jprorama@gmail.com>
2016-09-28 06:47:52 -0500

    Create a progress loading animation
    Modify the static progress bar representation to an animation of loading over time.
    
    Todo: animate fllling rather than draining progressbar effect

--- progressbar.js
+++ progressbar.js
@@ -11,5 +11,13 @@
  */
 function progressBar(percent, $element) {
 	var progressBarWidth = percent * $element.width() / 100;
-	$element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+	$element.find('div').animate({ width: progressBarWidth }, 1000).html(percent + "%&nbsp;");
+}
+
+function progressBarSpeed(speed, $element) {
+	var progressBarWidth = $element.width();
+	var percent = 100;
+	progressBarWidth = 0;
+	speed = speed * 1000;  // Convert seconds to milliseconds
+	$element.find('div').animate({ width: progressBarWidth }, speed).html(percent + "%&nbsp;");
 }
